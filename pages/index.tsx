5b90e9af346574e4801bff1aa13318a6d01e160f
importTahsinZaman <tahsinz21366@gmail.com>
2022-12-30 20:31:49 -0500

    First Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,123 +1,214 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useEffect, useRef } from "react";
+import styles from "../styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+import reactElementToJSXString from "react-element-to-jsx-string";
+
+import themes from "../public/themes.json";
 
 export default function Home() {
+  const terminal = useRef(null);
+
+  const executeCommand = (command: string) => {
+    let terminal_state = terminal.current.innerHTML;
+    terminal_state += newLineTextAsString(command);
+    terminal.current.innerHTML = terminal_state;
+
+    switch (command) {
+      case "help":
+        displayHelpMessage();
+        break;
+      case "clear":
+        clearTerminal();
+        break;
+      case "banner":
+        displayBanner();
+        break;
+      case "linkedin":
+        window.open(
+          "https://www.linkedin.com/in/tahsin-zaman-98761223a/",
+          "_blank",
+          "noopener,noreferrer"
+        );
+        break;
+      case "github":
+        window.open(
+          "https://github.com/importTahsinZaman",
+          "_blank",
+          "noopener,noreferrer"
+        );
+        break;
+      case "instagram":
+        window.open(
+          "https://www.instagram.com/tahsin__zaman/",
+          "_blank",
+          "noopener,noreferrer"
+        );
+        break;
+      case "email":
+        displayEmail();
+        break;
+      default:
+        displayErrorMessage(command);
+        break;
+    }
+  };
+
+  const displayHelpMessage = () => {
+    let terminal_state = terminal.current.innerHTML;
+    terminal_state += `<p>Available commands:</p>`;
+    terminal_state += `<p>about, banner, email, github, help, instagram, linkedin, projects, repo, whoami</p>`;
+    terminal.current.innerHTML = terminal_state;
+  };
+
+  const clearTerminal = () => {
+    terminal.current.innerHTML = "";
+  };
+
+  function delay(time: number) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+
+  const displayEmail = () => {
+    let terminal_state = terminal.current.innerHTML;
+    terminal_state += `<p>tahsinz21366@gmail.com</p>`;
+    terminal.current.innerHTML = terminal_state;
+  };
+
+  const displayBanner = () => {
+    let terminal_state = terminal.current.innerHTML;
+    let pointer = 0;
+    const addBannerLine = (pointer: number) => {
+      terminal_state += `<pre style="font-size:12px">${banner[pointer]}</pre>`;
+      terminal.current.innerHTML = terminal_state;
+    };
+    while (pointer < banner.length) {
+      addBannerLine(pointer);
+      pointer += 1;
+    }
+    const addBanner2Line = (pointer: number) => {
+      terminal_state += `<pre style="font-size:8px">${banner2[pointer]}</pre>`;
+      terminal.current.innerHTML = terminal_state;
+    };
+    pointer = 0;
+    while (pointer < banner2.length) {
+      addBanner2Line(pointer);
+      pointer += 1;
+    }
+    terminal_state += `</br>`;
+    terminal_state += `<p>Welcome!</p>`;
+    terminal_state += `<p>For a list of available commands, type 'help'.</p>`;
+    terminal.current.innerHTML = terminal_state;
+  };
+
+  const displayErrorMessage = (command: string) => {
+    let terminal_state = terminal.current.innerHTML;
+    terminal_state += `<p>Command not found: ${command}. Try 'help' to get started</p>`;
+    terminal.current.innerHTML = terminal_state;
+  };
+
+  const newLineText = (text: string) => {
+    return (
+      <div style={{ display: "flex" }}>
+        <p style={{ color: "var(--primary-color)" }}>guest</p>
+        <p>@</p>
+        <p style={{ color: "var(--secondary-color)" }}>tahsinzaman.dev:</p>
+        <pre>$ ~ </pre>
+        <p>{text}</p>
+      </div>
+    );
+  };
+
+  const newLineTextAsString = (text: string) => {
+    return `
+      <div style="display:flex; margin-top:5px; margin-bottom:5px">
+        <p style="color: var(--primary-color)">guest</p>
+        <p>@</p>
+        <p style="color: var(--secondary-color)">tahsinzaman.dev:</p>
+        <p>
+          <pre>$ ~ </pre>
+        </p>
+        <p>${text}</p>
+      </div>
+    `;
+  };
+
+  const banner = [
+    ``,
+    `                           .,,uod8B8bou,,.`,
+    `              ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:.`,
+    `         ,=m8BBBBBBBBBBBBBBBRPFT?!||||||||||||||`,
+    `         !...:!TVBBBRPFT||||||||||!!^^""'   ||||`,
+    `         !.......:!?|||||!!^^""'            ||||`,
+    `         !.........||||                     ||||`,
+    `         !.........||||                     ||||`,
+    `         !.........||||                     ||||`,
+    `         !.........||||                     ||||`,
+    `         !.........||||                     ||||`,
+    `         !.........||||                     ||||`,
+    `          .........||||                    ,||||`,
+    `          .;.......||||               _.-!!|||||`,
+    `   .,uodWBBBBb.....||||       _.-!!|||||||||!:'`,
+    ` YBBBBBBBBBBBBBBb..!|||:..-!!|||||||!iof68BBBBBb`,
+    ` ..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::    `,
+    `     ........::::::::::::::::;iof688888888888888888888b.      `,
+    `       ......:::::::::;iof688888888888888888888888888888b.`,
+    `         ....:::;iof688888888888888888888888888888888899fT!`,
+    `           ..::!8888888888888888888888888888888899fT|!^"'`,
+    `             ' !!988888888888888888888888899fT|!^"'`,
+    `                 !!8888888888888888899fT|!^"'`,
+    `                   !988888888899fT|!^"'`,
+  ];
+  const banner2 = [
+    `████████╗█████╗  ██╗  ██╗███████╗██╗███╗   ██╗    ███████╗ █████╗ ███╗   ███╗ █████╗ ███╗   ██╗`,
+    `╚══██╔══╝██╔══██╗██║  ██║██╔════╝██║████╗  ██║    ╚══███╔╝██╔══██╗████╗ ████║██╔══██╗████╗  ██║`,
+    `   ██║   ███████║███████║███████╗██║██╔██╗ ██║      ███╔╝ ███████║██╔████╔██║███████║██╔██╗ ██║`,
+    `   ██║   ██╔══██║██╔══██║╚════██║██║██║╚██╗██║     ███╔╝  ██╔══██║██║╚██╔╝██║██╔══██║██║╚██╗██║`,
+    `   ██║   ██║  ██║██║  ██║███████║██║██║ ╚████║    ███████╗██║  ██║██║ ╚═╝ ██║██║  ██║██║ ╚████║`,
+    `   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝    ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝`,
+  ];
+
+  useEffect(() => {
+    displayBanner();
+    let root = document.querySelector(":root");
+    const rand = Math.floor(Math.random() * themes.length);
+    const theme = themes[rand];
+    root.style.setProperty("--bg-color", theme.bgColor);
+    root.style.setProperty("--primary-color", theme.primaryColor);
+    root.style.setProperty("--primary-color", theme.secondaryColor);
+    root.style.setProperty("--font-color", theme.fontColor);
+  });
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>Tahsin Zaman</title>
+        <meta name="description" content="Tahsin Zaman's Personal Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+        <div className={styles.container}>
+          <div className={styles.container2}>
+            <div className={styles.terminal} ref={terminal}>
+              {newLineText("banner")}
+            </div>
+            <div className={styles.newline}>
+              {newLineText("")}
+              <input
+                className={styles.input}
+                rows={1}
+                maxLength={50}
+                onKeyUp={(e) => {
+                  if (e.key === "Enter") {
+                    executeCommand(e.target.value);
+                    e.target.value = "";
+                  }
+                }}
+              ></input>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
-  )
+  );
 }
